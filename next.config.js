aec6f04e954394539e3e35804489258083263c72
jackyef <jacky.efendi1@gmail.com>
2021-07-18 22:34:34 +0700

    Add service worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,31 @@
-const withPreact = require('next-plugin-preact')
+const withOffline = require('next-offline');
+const withPreact = require('next-plugin-preact');
+const { flowRight } = require('./src/utils/flow.js');
 
-module.exports = withPreact({
+const config = {
+  target: 'serverless',
+
   /* regular next.js config options here */
-})
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+
+    runtimeCaching: [
+      {
+        urlPattern: /.(png|jpg|jpeg|webp|svg|wav|mp3)$/,
+        handler: 'CacheFirst',
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = flowRight(withOffline, withPreact)(config);
