feebec636a932eb9a6169d5e3d4df2daf0f49a90
jackyef <jacky.efendi1@gmail.com>
2021-07-18 17:16:59 +0700

    Prefetch 2 audio assets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        {/* Prefetch the default 2 audio files */}
+        <link rel="prefetch" href="audio/rain.wav" />
+        <link rel="prefetch" href="audio/nature.wav" />
       </Head>
       <ThemeProvider>
         <Component {...pageProps} />
